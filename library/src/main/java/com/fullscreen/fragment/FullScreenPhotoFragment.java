342f13d25c262004afed73c9654341757211b5c9
Dmytro Manzhosov <manzhda@gmail.com>
2015-06-03 18:24:59 +0300

    Add license.

--- library/src/main/java/com/fullscreen/fragment/FullScreenPhotoFragment.java
+++ library/src/main/java/com/fullscreen/fragment/FullScreenPhotoFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Dmitriy Manzhosov, Yevgen Kulik, Dmitriy Kovalenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.fullscreen.fragment;
 
 import android.content.Intent;
@@ -6,7 +21,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +29,6 @@ import android.view.WindowManager;
 import com.fullscreen.ProgressShower;
 import com.fullscreen.ProgressShowerImpl;
 import com.fullscreen.R;
-import com.fullscreen.activity.FullScreenActivity;
 import com.fullscreen.activity.FullScreenActivity;
 import com.fullscreen.util.ImageLoaderUtils;
 import com.fullscreen.zoom.HideBarController;

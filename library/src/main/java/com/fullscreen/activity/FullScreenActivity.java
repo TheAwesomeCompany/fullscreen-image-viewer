54ad3117f10a077e76468c490d64baaa815c6fb6
IngvarMali <mali.ingvar@gmail.com>
2015-07-01 17:19:01 +0300

    Added viewPager for several photos

--- library/src/main/java/com/fullscreen/activity/FullScreenActivity.java
+++ library/src/main/java/com/fullscreen/activity/FullScreenActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 Dmitriy Manzhosov, Yevgen Kulik, Dmitriy Kovalenko
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.fullscreen.activity;
 
 import android.os.Bundle;
@@ -20,7 +5,6 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.fullscreen.R;
 import com.fullscreen.fragment.FullScreenPhotoFragment;
@@ -30,32 +14,27 @@ import com.fullscreen.fragment.FullScreenPhotoFragment;
  */
 public class FullScreenActivity extends ActionBarActivity {
 
-    public static final String BUNDLE_IMAGE = "bundle_image";
-    public static final String BUNDLE_STUB = "bundle_stub";
-
-    protected Toolbar mToolBar;
-
-    /**
-     * Fragment with showing data
-     */
-    protected Fragment mContentFragment;
-
-    /**
-     * Shadow under toolbar
-     */
-    private View mShadowView;
-
     /**
      * Parameters for hiding/showing toolbar and shadow
      */
     public int mToolBarHeight;
     public int mShadowHeight;
     public int mShortAnimTime;
+    protected Toolbar mToolBar;
+    /**
+     * Fragment with showing data
+     */
+    protected Fragment mContentFragment;
+    /**
+     * Shadow under toolbar
+     */
+//    private View mShadowView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
+
         if (savedInstanceState == null) {
             mContentFragment = onCreateContentFragment();
             getSupportFragmentManager().beginTransaction()
@@ -64,9 +43,11 @@ public class FullScreenActivity extends ActionBarActivity {
         } else {
             mContentFragment = getSupportFragmentManager().findFragmentById(R.id.container);
         }
+
         mToolBar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+
         mToolBar.setTitle(getTitle());
-        mShadowView = findViewById(R.id.toolbar_shadow);
+//        mShadowView = findViewById(R.id.toolbar_shadow);
         setSupportActionBar(mToolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         onInitToolBar(mToolBar);
@@ -80,7 +61,7 @@ public class FullScreenActivity extends ActionBarActivity {
         return R.layout.activity_toobar_overlay;
     }
 
-    protected Fragment onCreateContentFragment(){
+    protected Fragment onCreateContentFragment() {
         return new FullScreenPhotoFragment();
     }
 
@@ -96,26 +77,26 @@ public class FullScreenActivity extends ActionBarActivity {
      * Hide action bar and shadow under it
      */
     public void moveActionBar(boolean visible) {
-        if(mToolBarHeight == 0){
+        if (mToolBarHeight == 0) {
             mToolBarHeight = mToolBar.getTop() + mToolBar.getHeight();//getStatusBarHeight();
         }
-        if(mShadowHeight == 0){
-            mShadowHeight = mShadowView.getTop() + mShadowView.getHeight();
-        }
-        if(mShortAnimTime == 0) {
+//        if (mShadowHeight == 0) {
+//            mShadowHeight = mShadowView.getTop() + mShadowView.getHeight();
+//        }
+        if (mShortAnimTime == 0) {
             mShortAnimTime = getResources().getInteger(
                     android.R.integer.config_shortAnimTime);
         }
         mToolBar.animate().translationY(visible ? 0 : -mToolBarHeight)
                 .setDuration(mShortAnimTime);
-        mShadowView.animate().translationY(visible ? 0 : -mShadowHeight)
-                .setDuration(mShortAnimTime);
+//        mShadowView.animate().translationY(visible ? 0 : -mShadowHeight)
+//                .setDuration(mShortAnimTime);
 
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             onBackPressed();
             return true;
         }
